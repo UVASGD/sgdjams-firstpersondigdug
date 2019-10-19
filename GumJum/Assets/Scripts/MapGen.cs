b92b20f8bc7b00d4e7a6267b71463925fef710c6
Simon Whittle <smw2ed@virginia.edu>
2019-10-18 20:55:04 -0400

    FuCk

---
+++ GumJum/Assets/Scripts/MapGen.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGen : MonoBehaviour
+{
+
+    [SerializeField]
+    GameObject prefab;
+
+    [SerializeField]
+    float breadth = 1f;
+
+    [SerializeField]
+    Vector3Int mapDimens;
+
+    GameObject[,,] map;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Allocate();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Allocate()
+    {
+        map = new GameObject[mapDimens.x, mapDimens.y, mapDimens.z];
+
+        for (int x=0; x<mapDimens.x; x++)
+        {
+            for (int y=0; y<mapDimens.y; y++)
+            {
+                for (int z=0; z < mapDimens.z; z++)
+                {
+                    GameObject obj = Instantiate(prefab, transform);
+                    map[x, y, z] = obj;
+
+                    obj.transform.localPosition = new Vector3(x, y, z) * breadth;
+                }
+            }
+        }
+    }
+}

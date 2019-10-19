a97f86e1d6f62167236a8e9675607ac08a2b4834
ryanakann <ryanakann@gmail.com>
2019-10-19 15:34:50 -0400

    Initial monster AI complete

--- GumJum/Assets/Monster.cs
+++ GumJum/Assets/Monster.cs
@@ -8,26 +8,30 @@ public enum MonsterState {
 	chasing
 }
 
+[RequireComponent(typeof(Animator))]
 public class Monster : MonoBehaviour {
 	public GameObject player;
 	public bool seesPlayer;
+	public bool canBeginPhasing;
+	public bool insideWall;
 	private bool playerRaycast;
-	public bool up;
-	public bool down;
-	public bool left;
-	public bool right;
-	public bool forward;
-	public bool back;
+	private bool up;
+	private bool down;
+	private bool left;
+	private bool right;
+	private bool forward;
+	private bool back;
 
 	MapGen mapGenerator;
+	Animator anim;
 
-	public RaycastHit playerHit;
-	public RaycastHit upHit;
-	public RaycastHit downHit;
-	public RaycastHit leftHit;
-	public RaycastHit rightHit;
-	public RaycastHit forwardHit;
-	public RaycastHit backHit;
+	private RaycastHit playerHit;
+	private RaycastHit upHit;
+	private RaycastHit downHit;
+	private RaycastHit leftHit;
+	private RaycastHit rightHit;
+	private RaycastHit forwardHit;
+	private RaycastHit backHit;
 
 	public MonsterState state;
 	private MonsterState stateLF;
@@ -36,12 +40,14 @@ public class Monster : MonoBehaviour {
 	public Vector3 targetPosition;
 	public Vector3 targetVelocity;
 
-	private float switchDirectionsCD;
+	private float distanceSinceLastChangedDirection;
+	private float timePhasing;
+	private float timeUntilPhase;
 	
 
 	private void Start () {
 		seesPlayer = false;
-		//transform.forward = Random.onUnitSphere;
+		canBeginPhasing = false;
 
 		if (!player) {
 			player = GameObject.FindWithTag("Player");
@@ -51,7 +57,11 @@ public class Monster : MonoBehaviour {
 			mapGenerator = FindObjectOfType<MapGen>();
 		}
 
-		switchDirectionsCD = 0f;
+		distanceSinceLastChangedDirection = 999f;
+		anim = GetComponent<Animator>();
+		timePhasing = 0f;
+
+		timeUntilPhase = Random.Range(7f, 12f);
 	}
 
 	private void Update () {
@@ -60,6 +70,17 @@ public class Monster : MonoBehaviour {
 		UpdateStates();
 		StateController();
 		transform.position += transform.forward * speed * Time.deltaTime;
+		distanceSinceLastChangedDirection += speed * Time.deltaTime;
+	}
+
+	private void ChangeDirections (Vector3 direction) {
+		if (distanceSinceLastChangedDirection < 1f)
+			return;
+		distanceSinceLastChangedDirection = 0f;
+		if (direction != Vector3.up) {
+			transform.up = Vector3.up;
+		}
+		transform.forward = direction;
 	}
 
 	private void ShootRaycasts () {
@@ -99,105 +120,83 @@ public class Monster : MonoBehaviour {
 				if (stateLF != MonsterState.patrolling) {
 
 				}
-				//targetPosition = forwardHit.point;
-				//targetVelocity = (targetPosition - transform.position).normalized * speed;
-
-
-				//Switch directions
-				//if (forward) {
-				//	print("Distance: " + forwardHit.distance);
-				//}
-
-				//if (switchDirectionsCD > 0f) {
-				//	break;
-				//} else {
-				//	switchDirectionsCD -= Time.deltaTime;
-				//}
 
 				if (Physics.Raycast(transform.position, transform.forward, 1f, LayerMask.GetMask("Wall"))) {
-					//if (forward && forwardHit.distance < 1f) {
-					print("SWITCH DIRECTIONS!");
-					switchDirectionsCD = 0.5f;
-
-					//Debug.Log("Up: %d, Down: %d, Left: %d, Right: %d, Forward: %d, Back: %d",
-					//	  upHit.distance, downHit.distance, leftHit.distance, rightHit.distance, forwardHit.distance, backHit.distance);
-					Debug.Log("Up: " + upHit.distance + " Down: " + downHit.distance +
-							  " Left: " + leftHit.distance + " Right: " + rightHit.distance +
-							  " Forward: " + forwardHit.distance + " Back: " + backHit.distance);
-
 					List<Vector3> directions = new List<Vector3>();
 					if (up && upHit.distance > 1f && transform.forward != Vector3.up && transform.forward != Vector3.down) {
-						print("Up");
 						directions.Add(Vector3.up);
 					} else if (!up) {
 						directions.Add(Vector3.up);
 					}
 					if (down && downHit.distance > 1f && transform.forward != Vector3.down && transform.forward != Vector3.up) {
-						print("Down");
 						directions.Add(Vector3.down);
 					} else if (!down) {
 						directions.Add(Vector3.down);
 					}
 					if (left && leftHit.distance > 1f && transform.forward != Vector3.left && transform.forward != Vector3.right) {
-						print("Left");
 						directions.Add(Vector3.left);
 					} else if (!left) {
 						directions.Add(Vector3.left);
 					}
 					if (right && rightHit.distance > 1f && transform.forward != Vector3.right && transform.forward != Vector3.left) {
-						print("Right");
 						directions.Add(Vector3.right);
 					} else if (!right) {
 						directions.Add(Vector3.right);
 					}
 					if (forward && forwardHit.distance > 1f && transform.forward != Vector3.forward && transform.forward != Vector3.back) {
-						print("Forward");
 						directions.Add(Vector3.forward);
 					} else if (!forward) {
 						directions.Add(Vector3.forward);
 					}
 					if (back && backHit.distance > 1f && transform.forward != Vector3.back && transform.forward != Vector3.forward) {
-						print("Back");
 						directions.Add(Vector3.back);
 					} else if (!back) {
 						directions.Add(Vector3.back);
 					}
 
-					print("Options: " + directions.Count);
 					if (directions.Count == 0) {
-						print("!!!!!");
 						transform.forward = -transform.forward;
 					} else {
 						Vector3 direction = directions[Random.Range(0, directions.Count)];
-						print("Setting Forward from " + transform.forward + " to " + direction);
-						if (transform.forward != Vector3.up) {
-							transform.up = Vector3.up;
-						}
-						print("Before: " + transform.forward);
-						transform.forward = direction;
-						print("After: " + transform.forward);
-						
-						print("Afterer: " + transform.forward);
+						ChangeDirections(direction);
 					}
-				} else {
-					//print("NO WALL");
 				}
-				//targetVelocity = transform.forward * speed;
 				break;
 			case MonsterState.phasing:
-				if (stateLF != MonsterState.phasing) {
+				//if (stateLF != MonsterState.phasing) {
 					
-				}
-				targetPosition = player.transform.position;
-				targetVelocity = (targetPosition - transform.position).normalized * speed;
-				if (!Physics.CheckSphere(transform.position, 0.2f, LayerMask.GetMask("Wall"))) {
-					state = MonsterState.patrolling;
-				}
-				break;
+				//}
+				
+				//break;
 			case MonsterState.chasing:
-				if (stateLF != MonsterState.chasing) {
+				//if (stateLF != MonsterState.chasing) {
 
+				//}
+
+				Vector3 targetDirection = Vector3.up;
+				float playerDot = Vector3.Dot(Vector3.up, player.transform.position - transform.position);
+				if (Vector3.Dot(Vector3.down, player.transform.position - transform.position) > playerDot) {
+					playerDot = Vector3.Dot(Vector3.down, player.transform.position - transform.position);
+					targetDirection = Vector3.down;
 				}
+				if (Vector3.Dot(Vector3.left, player.transform.position - transform.position) > playerDot) {
+					playerDot = Vector3.Dot(Vector3.left, player.transform.position - transform.position);
+					targetDirection = Vector3.left;
+				}
+				if (Vector3.Dot(Vector3.right, player.transform.position - transform.position) > playerDot) {
+					playerDot = Vector3.Dot(Vector3.right, player.transform.position - transform.position);
+					targetDirection = Vector3.right;
+				}
+				if (Vector3.Dot(Vector3.forward, player.transform.position - transform.position) > playerDot) {
+					playerDot = Vector3.Dot(Vector3.forward, player.transform.position - transform.position);
+					targetDirection = Vector3.forward;
+				}
+				if (Vector3.Dot(Vector3.back, player.transform.position - transform.position) > playerDot) {
+					playerDot = Vector3.Dot(Vector3.back, player.transform.position - transform.position);
+					targetDirection = Vector3.back;
+				}
+
+				ChangeDirections(targetDirection);
 				break;
 			default:
 				break;
@@ -205,14 +204,47 @@ public class Monster : MonoBehaviour {
 	}
 
 	private void UpdateStates () {
+		//Sees player
 		ShootRaycasts();
-		//if (state != MonsterState.phasing) {
-		//	if (Random.value < 0.1f * Time.deltaTime) {
-		//		state = MonsterState.phasing;
-		//	}
-		//}
 
-		//stateLF = state;
+		//Inside wall
+		if (Physics.CheckSphere(transform.position, 0.2f, LayerMask.GetMask("Wall"))) {
+			insideWall = true;
+		} else {
+			insideWall = false;
+		}
+
+		//Phasing
+		if (state == MonsterState.patrolling) {
+			if (timeUntilPhase <= 0f) {
+				canBeginPhasing = true;
+			}
+			timeUntilPhase -= Time.deltaTime;
+			timePhasing = 0f;
+		} else if (state == MonsterState.chasing) {
+			timePhasing = 0f;
+		} else if (state == MonsterState.phasing) {
+			if (timePhasing < Time.deltaTime) {
+				timeUntilPhase = Random.Range(7f, 12f);
+			}
+
+			if (timePhasing > 3f) {
+				canBeginPhasing = false;
+			}
+			timePhasing += Time.deltaTime;
+		}
+
+		anim.SetBool("seesPlayer", seesPlayer);
+		anim.SetBool("canBeginPhasing", canBeginPhasing);
+		anim.SetBool("insideWall", insideWall);
+
+		if (anim.GetCurrentAnimatorStateInfo(0).IsName("Patrolling")) {
+			state = MonsterState.patrolling;
+		} else if (anim.GetCurrentAnimatorStateInfo(0).IsName("Chasing")) {
+			state = MonsterState.chasing;
+		} else if (anim.GetCurrentAnimatorStateInfo(0).IsName("Phasing")) {
+			state = MonsterState.phasing;
+		}
 	}
 
 	private void OnDrawGizmos () {

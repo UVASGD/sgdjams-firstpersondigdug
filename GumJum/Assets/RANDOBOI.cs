a2f84b8a975b60ee2db5cd03abc6d821f7d9608f
ryanakann <ryanakann@gmail.com>
2019-10-20 13:19:16 -0400

    aaihuegrijosgr

---
+++ GumJum/Assets/RANDOBOI.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RANDOBOI : MonoBehaviour
+{
+	Vector3 startPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		startPosition = transform.position;    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		transform.position = startPosition + Random.insideUnitSphere * 0.07f;
+    }
+}

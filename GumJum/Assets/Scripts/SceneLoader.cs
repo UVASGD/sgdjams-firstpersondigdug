ba8762e055eb36e55fe19b4543d4d10d19bb515a
Simon Whittle <smw2ed@virginia.edu>
2019-10-19 22:11:42 -0400

    added SceneLoader

---
+++ GumJum/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public static void MainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public static void Level()
+    {
+        SceneManager.LoadScene("Level");
+    }
+
+    public static void Credits()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+}

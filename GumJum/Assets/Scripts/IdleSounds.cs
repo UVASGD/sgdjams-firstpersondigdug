4671ad33cfd120f83739d2ba6456cc9011b38472
Wyatt Joyner <wyatt.joyner@practicalcode.us>
2019-10-20 12:34:27 -0400

    FX

---
+++ GumJum/Assets/Scripts/IdleSounds.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IdleSounds : MonoBehaviour
+{
+    public List<GameObject> sounds = new List<GameObject>();
+    public float soundTime = 10f;
+    bool playing;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!playing && sounds.Count > 0)
+        {
+            StartCoroutine(PlayIdle());
+        }
+    }
+
+    IEnumerator PlayIdle()
+    {
+        playing = true;
+        yield return new WaitForSeconds(Random.Range(1f, soundTime));
+        GameObject fx = sounds[Random.Range(0, sounds.Count)];
+        Instantiate(fx, transform.position, Quaternion.identity);
+        playing = false;
+    }
+}
